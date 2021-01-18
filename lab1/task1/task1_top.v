afd508f3e4d17482ee02f1f8773f90fa8b5e2b63
sts219 <sts219@ic.ac.uk>
2021-01-18 17:31:54 +0000

    Completed Lab1 Task1 -- initial commit

---
+++ lab1/task1/task1_top.v
@@ -0,0 +1,11 @@
+module task1_top (
+		SW,		// input switches
+		HEX0		//Hex output on 7 segment display
+	);
+	
+	input [3:0] SW; // Declare input/output ports
+	output [6:0] HEX0;
+	
+	hex_to_7seg SEG0 (HEX0, SW[3:0]);
+	
+endmodule
